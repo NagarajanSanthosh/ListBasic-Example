9babfa5fa48062bcbfa159cbda5586b53fb259aa
NagarajanSanthosh <64208ns@gmail.com>
2022-11-26 05:47:33 +0530

    List basic example

---
+++ ListArrayExample.java
@@ -0,0 +1,13 @@
+package basicExample;
+
+import java.util.*;
+
+public class ListArrayExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(5);
+        l.add(3);
+        l.add(8);
+        System.out.println(l);
+    }
+}
