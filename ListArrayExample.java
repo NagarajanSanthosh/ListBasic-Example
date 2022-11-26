13f80ea32ad30fe80e24740f5c38c76c44601a79
NagarajanSanthosh <64208ns@gmail.com>
2022-11-26 06:12:25 +0530

    modified with contains method

--- ListArrayExample.java
+++ ListArrayExample.java
@@ -4,10 +4,14 @@ import java.util.*;
 
 public class ListArrayExample {
     public static void main(String[] args) {
-        ArrayList<Integer> l = new ArrayList<>();
+        ArrayList l = new ArrayList<>();
         l.add(5);
         l.add(3);
         l.add(8);
+        l.add("X");
+        l.add(true);
+        l.add(0,10);
         System.out.println(l);
+        System.out.println("size of list is "+l.size()+"\ncheck whether or not element 10 is present "+l.contains(10));
     }
 }
